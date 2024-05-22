ddf73a54276ea63675e83d081c0632b34645410a
unknown <raghavendiran46461@gmail.com>
2024-05-22 19:29:33 +0530

    DB Context Initialized

---
+++ PharmacyManagementSystemSolution/PharmacyManagementSystem/Context/DBPharmacyContext.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyManagementSystem.Models.DBModels;
+
+namespace PharmacyManagementSystem.Context
+{
+    public class DBPharmacyContext : DbContext
+    {
+        public DBPharmacyContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {         
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Orders)
+                .WithOne(o => o.User)
+                .HasForeignKey(o => o.UserID);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Reviews)
+                .WithOne(r => r.User)
+                .HasForeignKey(r => r.UserID);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.ShoppingCarts)
+                .WithOne(sc => sc.User)
+                .HasForeignKey(sc => sc.UserID);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryID);
+
+            modelBuilder.Entity<Discount>()
+                .HasMany(d => d.Products)
+                .WithOne(p => p.Discount)
+                .HasForeignKey(p => p.DiscountID);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(p => p.OrderItems)
+                .WithOne(oi => oi.Product)
+                .HasForeignKey(oi => oi.ProductID);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(p => p.Reviews)
+                .WithOne(r => r.Product)
+                .HasForeignKey(r => r.ProductID);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(p => p.ShoppingCarts)
+                .WithOne(sc => sc.Product)
+                .HasForeignKey(sc => sc.ProductID);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.OrderItems)
+                .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderID);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Payments)
+                .WithOne(p => p.Order)
+                .HasForeignKey(p => p.OrderID);
+        }
+    }
+}
